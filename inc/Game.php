92ce4b3fa63b253f1cd5e1fb61329fc947bc540d
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-11-06 13:28:25 -0500

    Update Game.php
    Add properties

--- inc/Game.php
+++ inc/Game.php
@@ -2,4 +2,7 @@
 // Game.php to create a Game class with methods for showing the game, handling interactions, and checking for game over.
 
 class Game 
-{}
+{
+	private $phrase;
+	private $lives = 5;
+}
