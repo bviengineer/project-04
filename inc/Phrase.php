97f1c2e9eaadca6b3dbc9cdebb0343a0984cb352
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-11-06 11:25:38 -0500

    Update Phrase.php
    Add constuctor method

--- inc/Phrase.php
+++ inc/Phrase.php
@@ -2,4 +2,17 @@
 // Phrase.php to create a Phrase class to handle the phrases
 
 class Phrase 
-{}
+{
+	private $currentPhrase = "dream big"; //"Australia is known as the land Down Under";
+	private $selected = [];
+
+	public function __construct($currentPhrase = null, $selected = array() ) {
+		if (isset($currentPrhase) && isset($selected) ) {
+			$this->currentPhrase = $currentPhrase;
+			$this->selected = $selected;
+		} elseif(!isset($currentPhrase) && !isset($selected)) {
+			$this->currentPhrase = $this->currentPhase;
+		}
+	}
+
+}
