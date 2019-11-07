ba987b518928c9ef77b1a277976a6a50922b9278
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-11-06 22:05:47 -0500

    Update Phrase.php
    -Correct parameter name in conditional statement.
    -Pass string to parameter to test object
    -Change logical operator to ||

--- inc/Phrase.php
+++ inc/Phrase.php
@@ -6,8 +6,8 @@ class Phrase
 	private $currentPhrase = "dream big"; //"Australia is known as the land Down Under";
 	private $selected = [];
 
-	public function __construct($currentPhrase = null, $selected = null ) {
-		if (!empty($currentPrhase) && !empty($selected) ) {
+	public function __construct($currentPhrase = "Australia is known as the land Down Under", $selected = array() ) {
+		if (!empty($currentPhrase) || !empty($selected) ) {
 			$this->currentPhrase = $currentPhrase;
 			$this->selected = $selected;
 		} //elseif(empty($currentPhrase) && empty($selected)) {
