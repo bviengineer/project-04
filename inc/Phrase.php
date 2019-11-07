d416ace46dd2b9036cb0ca2da1d54ac78021b6bd
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-11-07 04:40:30 -0500

    Update Phrase.php
    -Convert phrase string to array, loop through the array and echo each letter as an li with a specified class

--- inc/Phrase.php
+++ inc/Phrase.php
@@ -5,6 +5,7 @@ class Phrase
 {
 	private $currentPhrase; //= "dream big"; //"Australia is known as the land Down Under";
 	private $selected = [];
+	private $myPhrase; // holds return string from addPhraseToDisplay()
 
 	public function __construct($currentPhrase = null, $selected = array() ) {
 		if (!empty($currentPhrase) || !empty($selected) ) {
@@ -18,6 +19,17 @@ class Phrase
 	// Adds letter placeholders to the display when game begins
 	public function addPhraseToDisplay() {
 		$characters = str_split(strtolower($this->currentPhrase));
-		return $characters;
+		echo "<ul>";
+			foreach ($characters as $character) {
+				$this->myPhrase .= $character;
+				if ($character == " ") {
+					echo "<li class='space'>" . $character . "</li>";
+				} else {
+						echo "<li class='hide letter'>" . $character . "</li>";
+				}
+			}
+		echo "</ul>";
+		//return $character; returns only the last letter of the array as a string: "g"
+		//return $this->myPhrase;
 	}
 }
