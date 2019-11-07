6c34389261a60975c73f2f657227815d496ec104
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-11-07 09:50:48 -0500

    Update play.php
    -Step #5 of project instructions

--- play.php
+++ play.php
@@ -1,19 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Phrase Hunter</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/styles.css" rel="stylesheet">
-    <link href="css/animate.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-</head>
+	<head>
+		<meta charset="utf-8">
+		<title>Phrase Hunter</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="css/styles.css" rel="stylesheet">
+		<link href="css/animate.css" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	</head>
 
-<body>
-	<div class="main-container">
-			<div id="banner" class="section">
-					<h2 class="header">Phrase Hunter</h2>
-			</div>
+	<body>
+		<div class="main-container">
+			<h2 class="header">Phrase Hunter</h2>
 			<?php // play.php to haxndle the HTML, instantiating objects, storing sessions and calling appropriate methods 
 				include "inc/Game.php";
 				include 'inc/Phrase.php';
@@ -29,7 +27,7 @@
 				// echo "<pre>";
 				// var_dump($game);
 				// echo "</pre>";
-			?>
-	</div>
-</body>
+			?>    
+		</div>
+	</body>
 </html>
