08f1c7c088ed62b6b079b77e394b84c1438681b7
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-11-08 13:45:34 -0500

    Update play.php
    -Uncomment session variable assignemnt
    -Pass $_SESSION['phrase'] & $_SESSION['selected'] to instance of Phrase class
    -Pass Phrase instance to $game object
    -Fix spacing
    -Move comment

--- play.php
+++ play.php
@@ -1,17 +1,17 @@
 <?php 
+// play.php to haxndle the HTML, instantiating objects, storing sessions and calling appropriate methods
 	session_start();
-
+	
 	include "inc/Game.php";
 	include 'inc/Phrase.php';
-	
+
 	if (isset($_SESSION['selected']) && isset($_POST['input'])) {
 		array_push($_SESSION['selected'], filter_input(INPUT_POST, 'input', FILTER_SANITIZE_STRING));
 	} else {
 		$_SESSION['selected'] = [];
 	}
-
-	//$_SESSION['phrase'] = 'start small';
-	var_dump($_SESSION);
+	$_SESSION['phrase'] = 'start small';
+	//var_dump($_SESSION);
 	//session_destroy();
 ?>
 <!DOCTYPE html>
@@ -28,25 +28,24 @@
 	<body>
 		<div class="main-container">
 			<h2 class="header">Phrase Hunter</h2>
-			<?php // play.php to haxndle the HTML, instantiating objects, storing sessions and calling appropriate methods 
-		
-				$phrase = new Phrase();
+			<?php 
+				$phrase = new Phrase($_SESSION['phrase'], $_SESSION['selected']);
 				// echo "<pre>";
 				// var_dump($phrase);
 				// echo "</pre>";
 
 				echo $phrase->addPhraseToDisplay();
 
-				$game = new Game($_SESSION);
-				// echo "<pre>";
-				// var_dump($game);
-				// echo "</pre>";
+				$game = new Game($phrase);
+				//echo "<pre>";
+				var_dump($game);
+				//echo "</pre>";
 			?>
 				<form method='POST' action='play.php'>
 					<?php
 						echo $game->displayKeyboard();
 						//echo "<br>";
-						var_dump($_POST);
+						//var_dump($_POST);
 					?>
 				</form>
 				<?php 
