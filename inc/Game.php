798b53cfc41dfa0105c044ed0dc5e21ea2bbdaf6
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-11-14 09:49:53 -0500

    Update Game.php
    -Refactor displayKeyboard() method
    -Create an independent loop for each row of keyboard that's called by displayKeyboard()

--- inc/Game.php
+++ inc/Game.php
@@ -13,6 +13,8 @@ class Game
 	private $bottom_row = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
 	private $keyboard = [];
 	private $keys;
+	private $openDivTag = "<div class='keyrow'>";
+	private $closeDivTag = "</div>";
 
 	public function __construct($phraseObject) {
 		$this->phrase = $phraseObject;
@@ -20,29 +22,40 @@ class Game
 
 	// Prints keyboard to the page
 	public function displayKeyboard() {
-		$this->keys .= "<div class='keyrow'>";
-		foreach ($this->top_row as $f_row) {
-			$this->keyboard();
-			$this->keys .= "<button class='key' name='input' value='" . $f_row . "'>" . $f_row . "</button>";	
-		}
-		$this->keys .= "</div>";
+		$this->keys .= $this->openDivTag;
+		$this->rowOneKeys();
+		$this->keys .= $this->closeDivTag;
 
-		$this->keys .= "<div class='keyrow'>";
-		foreach ($this->middle_row as $m_row) {
-			$this->keyboard();
-			$this->keys .= "<button class='key' name='input' value='" . $m_row . "'>" . $m_row . "</button>";	
-		}
-		$this->keys .= "</div>";
+		$this->keys .= $this->openDivTag;
+		$this->rowTwoKeys();
+		$this->keys .= $this->closeDivTag;
 
-		$this->keys .= "<div class='keyrow'>";
-		foreach ($this->bottom_row as $b_row) {
-			$this->keyboard();
-			$this->keys .= "<button class='key' name='input' value='" . $b_row . "'>" . $b_row . "</button>";	
-		}
-		$this->keys .= "</div>";
+		$this->keys .= $this->openDivTag;
+		$this->rowThreeKeys();
+		$this->keys .= $this->closeDivTag;
+
 		return $this->keys;
 	}
-
+	// Loops through and returns first row of keyboard 
+	public function rowOneKeys() {
+		foreach ($this->top_row as $f_row) {
+			$this->keys .= "<button class='key' name='input' value='" . $f_row . "'>" . $f_row . "</button>";
+		}
+		return $this->keys;
+	}
+	// Loops through and returns second row of keyboard
+	public function rowTwoKeys(){
+		foreach ($this->middle_row as $m_row) {
+			$this->keys .= "<button class='key' name='input' value='" . $m_row . "'>" . $m_row . "</	button>";	
+		}
+		return $this->keys;
+	}
+	// Loops through and returns third row of keyboard
+	public function rowThreeKeys() {
+		foreach ($this->bottom_row as $b_row) {
+			$this->keys .= "<button class='key' name='input' value='" . $b_row . "'>" . $b_row . "</button>";	
+		}
+	}
 	public function displayScore() {
 		$this->hearts = "<div id='scoreboard' class='section'><ol>";
 		for ($i = 0; $i <= $this->lives-1; $i++) {
@@ -62,15 +75,17 @@ class Game
 
 		foreach ($this->keyboard as $key) {
 			if (!in_array($key, $selectedLetters)) {
-					$this->keys .= "<button class='key' name='input' value='" . $key . "'>" . $key . "</button>";
+					// $this->keys .= "<button class='key' name='input' value='" . $key . "'>" . $key . "</button>";
+					echo $key . " player didn't select a key <br>";
 			}
 			if (in_array($key, $selectedLetters) && $this->phrase->checkLetter($key) == false) {
-					$this->keys .= "<button class='key incorrect' name='input' value='" . $key .  "' disabled>" . $key . "</button>";
+					// $this->keys .= "<button class='key incorrect' name='input' value='" . $key .  "' disabled>" . $key . "</button>";
+					echo $key . " is not a correct guess <br>";
 			} 
 			if (in_array($key, $selectedLetters) && $this->phrase->checkLetter($key) == true) {
-					$this->keys .=  "<button class='key correct' name='input' value='" . $key .  "' disabled>" . $key . "</button>";
-			} 
-				//return $this->keys;
+					// $this->keys .=  "<button class='key correct' name='input' value='" . $key .  "' disabled>" . $key . "</button>";
+					echo $key . " is a correct guess <br>";
+			}
 		}
 	}
 }
