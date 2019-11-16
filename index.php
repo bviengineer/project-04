93b6d62d113480e9a5cd9cde7a11fc6f2778ceca
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-11-16 14:14:07 -0500

    Update index.php
    change value of name attribute to start

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 		<div class="main-container">
 			<h2 class="header">Phrase Hunter</h2>
             <form action="play.php" method="POST">
-                <input id="btn__reset" type="submit" name="play" value="Start Game" />
+                <input id="btn__reset" type="submit" name="start" value="Start Game" />
             </form>
 		</div>
 
