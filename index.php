7f29589b58fd4a18847537c773b93a8e13a1bd6e
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-11-15 20:54:50 -0500

    Update index.php
    Add method attribute to form

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 	<body>
 		<div class="main-container">
 			<h2 class="header">Phrase Hunter</h2>
-            <form action="play.php">
+            <form action="play.php" method="POST">
                 <input id="btn__reset" type="submit" name="play" value="Start Game" />
             </form>
 		</div>
