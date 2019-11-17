34186edb1c78971405ca656695f03d25b9c080e9
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-11-17 12:28:09 -0500

    game_over.php
    Move game_over.php to root of folder

--- game_over.php
+++ game_over.php
@@ -2,13 +2,12 @@
 	session_start();
 	// session_destro();
 	include 'Phrase.php';
-
 ?>
 
 <!-- <h1 id="game-over-message">Congratulations on guessing: "The adventure begins"</h1> -->
 
 <h1 id="game-over-message">The phrase was: <?php echo $_SESSION['phrase']; ?>. Better luck next time!
-    <form action="/play.php" method="POST">
-        <input id="btn__reset" type="submit" name="start" value="Play again!" />
-    </form>
+  <form action="/play.php" method="POST">
+    <input id="btn__reset" type="submit" name="start" value="Play again!" />
+  </form>
 </h1>
