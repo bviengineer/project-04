04fb2b26d6bb2bfd252ad617df621a1a24e5a046
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-11-17 12:26:23 -0500

    Update game_over.php
    Start session on page and use phrase stored in session variable to output phrase user was unable to guess

--- inc/game_over.php
+++ inc/game_over.php
@@ -1,3 +1,14 @@
-<h1 id="game-over-message">Congratulations on guessing: "The adventure begins"</h1>
+<?php 
+	session_start();
+	// session_destro();
+	include 'Phrase.php';
 
-<h1 id="game-over-message">The phrase was: "The adventure begins". Better luck next time!</h1>
+?>
+
+<!-- <h1 id="game-over-message">Congratulations on guessing: "The adventure begins"</h1> -->
+
+<h1 id="game-over-message">The phrase was: <?php echo $_SESSION['phrase']; ?>. Better luck next time!
+    <form action="/play.php" method="POST">
+        <input id="btn__reset" type="submit" name="start" value="Play again!" />
+    </form>
+</h1>
