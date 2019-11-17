a27be0ac14001b03b6b8dff95c07100a5e231e8b
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-11-17 12:54:28 -0500

    Update game_over.php
    add div and h2 tags with title, to page

--- game_over.php
+++ game_over.php
@@ -10,9 +10,11 @@
 ?>
 
 <!-- <h1 id="game-over-message">Congratulations on guessing: "The adventure begins"</h1> -->
-
+<div class="main-container">
+			<h2 class="header">Phrase Hunter</h2>
 <h1 id="game-over-message">The phrase was: <?php echo $_SESSION['phrase']; ?>. Better luck next time!
   <form action="play.php" method="POST">
     <input id="btn__reset" type="submit" name="start" value="Play again!" />
   </form>
+  </div>
 </h1>
