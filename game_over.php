46e2ee1c278495042a366318cee7df644aa25d70
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-11-17 12:39:01 -0500

    Update game_over.php
    Add game resets to page

--- game_over.php
+++ game_over.php
@@ -1,13 +1,18 @@
 <?php 
 	session_start();
 	// session_destro();
-	include 'Phrase.php';
+
+  // Resets the game
+	if (isset($_POST['start'])) {
+    unset($_SESSION['selected']);
+    unset($_SESSION['phrase']);
+  }
 ?>
 
 <!-- <h1 id="game-over-message">Congratulations on guessing: "The adventure begins"</h1> -->
 
 <h1 id="game-over-message">The phrase was: <?php echo $_SESSION['phrase']; ?>. Better luck next time!
-  <form action="/play.php" method="POST">
+  <form action="play.php" method="POST">
     <input id="btn__reset" type="submit" name="start" value="Play again!" />
   </form>
 </h1>
