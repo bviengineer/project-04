f098a9f30f64a54877a7d78d37585be0b5a4b136
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-11-18 11:54:18 -0500

    Update Game.php
    -Change visibility of public properties to private
    -Fix indentation
    -Add comments

--- inc/Game.php
+++ inc/Game.php
@@ -12,10 +12,10 @@ class Game
 	private $top_row = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'];
 	private $middle_row = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'];
 	private $bottom_row = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
-	public 	$keys;
-	public 	$topRowKeys;
-	public 	$middleRowKeys;
-	public 	$bottomRowKeys;
+	private $keys; // holds html for keyboard 
+	private $topRowKeys; // holds keys with html tags for 1st row of keyboard to pass to $keys
+	private $middleRowKeys; // holds keys with html tags for 2nd row of keyboard to pass to $keys
+	private $bottomRowKeys; // holds keys with html tags for 3rd row of keyboard to pass to $keys
 	private $openDivTag = "<div class='keyrow'>";
 	private $closeDivTag = "</div>";
 
@@ -24,52 +24,52 @@ class Game
 		$this->phrase = $phraseObject;
 	}
 
-	// Assembles game keyboard by row
-	public function displayKeyboard() {
-		$this->keys .= $this->openDivTag;
-		$this->rowOneKeys();
-		$this->keys .= $this->closeDivTag;
+// Assembles game keyboard by row
+public function displayKeyboard() {
+	$this->keys .= $this->openDivTag;
+	$this->rowOneKeys();
+	$this->keys .= $this->closeDivTag;
 
-		$this->keys .= $this->openDivTag;
-		$this->rowTwoKeys();
-		$this->keys .= $this->closeDivTag;
+	$this->keys .= $this->openDivTag;
+	$this->rowTwoKeys();
+	$this->keys .= $this->closeDivTag;
 
-		$this->keys .= $this->openDivTag;
-		$this->rowThreeKeys();
-		$this->keys .= $this->closeDivTag;
+	$this->keys .= $this->openDivTag;
+	$this->rowThreeKeys();
+	$this->keys .= $this->closeDivTag;
 
-		return $this->keys;
-	}
+	return $this->keys;
+}
 
-	// Loops through and returns first row of keyboard 
-	public function rowOneKeys() {
-		foreach ($this->top_row as $f_row) {
-			if (!in_array($f_row, $this->phrase->selected)) {
-					$this->topRowKeys .= "<button type='submit' class='key' name='input' value='"; 
-					$this->topRowKeys .= $f_row . "'>" . $f_row . "</button>";
-			}
-			if (in_array($f_row, $this->phrase->selected) && $this->phrase->checkLetter($f_row) == true) {
-					$this->topRowKeys .= "<button type='submit' class='key chosen' name='input' value='";
-					$this->topRowKeys .= $f_row . "' disabled>" . $f_row . "</button>";
-			}
-			if (in_array($f_row, $this->phrase->selected) && $this->phrase->checkLetter($f_row) == false) {
-				$this->topRowKeys .= "<button type='submit' class='key wrong' name='input' value='"; 
-				$this->topRowKeys .= $f_row . "' disabled>" . $f_row . "</button>";
-			}		
+// Loops through and returns first row of keyboard 
+public function rowOneKeys() {
+	foreach ($this->top_row as $f_row) {
+		if (!in_array($f_row, $this->phrase->selected)) {
+				$this->topRowKeys .= "<button type='submit' class='key' name='input' value='"; 
+				$this->topRowKeys .= $f_row . "'>" . $f_row . "</button>";
 		}
-		return $this->keys .= $this->topRowKeys;
+		if (in_array($f_row, $this->phrase->selected) && $this->phrase->checkLetter($f_row) == true) {
+				$this->topRowKeys .= "<button type='submit' class='key chosen' name='input' value='";
+				$this->topRowKeys .= $f_row . "' disabled>" . $f_row . "</button>";
+		}
+		if (in_array($f_row, $this->phrase->selected) && $this->phrase->checkLetter($f_row) == false) {
+			$this->topRowKeys .= "<button type='submit' class='key wrong' name='input' value='"; 
+			$this->topRowKeys .= $f_row . "' disabled>" . $f_row . "</button>";
+		}		
 	}
+	return $this->keys .= $this->topRowKeys;
+}
 
-	// Loops through and returns second row of keyboard
-	public function rowTwoKeys(){
-		foreach ($this->middle_row as $m_row) {
-			if (!in_array($m_row, $this->phrase->selected)) {
-				$this->middleRowKeys .= "<button type='submit' class='key' name='input' value='";
-				$this->middleRowKeys .= $m_row . "'>" . $m_row . "</button>";
+// Loops through and returns second row of keyboard
+public function rowTwoKeys(){
+	foreach ($this->middle_row as $m_row) {
+		if (!in_array($m_row, $this->phrase->selected)) {
+			$this->middleRowKeys .= "<button type='submit' class='key' name='input' value='";
+			$this->middleRowKeys .= $m_row . "'>" . $m_row . "</button>";
 		}
 		if (in_array($m_row, $this->phrase->selected) && $this->phrase->checkLetter($m_row) == true) {
-				$this->middleRowKeys .= "<button type='submit' class='key chosen' name='input' value='";
-				$this->middleRowKeys .= $m_row . "' disabled>" . $m_row . "</button>";
+			$this->middleRowKeys .= "<button type='submit' class='key chosen' name='input' value='";
+			$this->middleRowKeys .= $m_row . "' disabled>" . $m_row . "</button>";
 		}
 		if (in_array($m_row, $this->phrase->selected) && $this->phrase->checkLetter($m_row) == false) {
 			$this->middleRowKeys .= "<button type='submit' class='key wrong' name='input' value='";
@@ -87,15 +87,15 @@ public function rowThreeKeys() {
 			$this->bottomRowKeys .= $b_row . "'>" . $b_row . "</button>";
 		}
 		if (in_array($b_row, $this->phrase->selected) && $this->phrase->checkLetter($b_row) == true) {
-				$this->bottomRowKeys .= "<button type='submit' class='key chosen' name='input' value='";
-				$this->bottomRowKeys .= $b_row . "' disabled>" . $b_row . "</button>";
+			$this->bottomRowKeys .= "<button type='submit' class='key chosen' name='input' value='";
+			$this->bottomRowKeys .= $b_row . "' disabled>" . $b_row . "</button>";
 		}
 		if (in_array($b_row, $this->phrase->selected) && $this->phrase->checkLetter($b_row) == false) {
 			$this->bottomRowKeys .= "<button type='submit' class='key wrong' name='input' value='";
 			$this->bottomRowKeys .= $b_row . "' disabled>" . $b_row . "</button>";
 		}
 	}
-	return $this->keys .= $this->bottomRowKeys;
+		return $this->keys .= $this->bottomRowKeys;
 }
 
 // Calculates and returns player score 
@@ -107,46 +107,48 @@ public function displayScore() {
 	$this->lives = $this->lives - $this->phrase->numberLost(); // updating # of wins based on losses 
 
 	if ($this->lives == 5) {
-			for ($win = 1; $win <= $this->lives; $win++) {
-						$this->hearts .= $winHearts;
-			} 
-		} elseif ($this->lives >= 0 && $this->lives < 5 && $this->phrase->numberLost() >= 1 && $this->phrase->numberLost() <= 5) {
-				for ($win = 1; $win <= $this->lives; $win++) {
-					$this->hearts .= $winHearts;
-				}
-				for ($lose = 1; $lose <= $this->phrase->numberLost(); $lose++) {
-					$this->hearts .= $loseHearts;
-				}
-		}
-	 	$this->hearts .= "</ol></div>";
-	 	return $this->hearts;
-	}
-
-	// Checks whether player lost the game
-	public function checkForLose() {
-		if ($this->phrase->numberLost() == 5) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	// Redirects to lose page if player lost game or to win page if player won game
-	public function gameOver() {
-		if ($this->checkForLose()) {
-				header('Location: game_over.php');
-		} elseif ($this->checkForWin()) {
-				header('Location: game_win.php');
+		for ($win = 1; $win <= $this->lives; $win++) {
+			$this->hearts .= $winHearts;
 		} 
+	} elseif ($this->lives >= 0 && $this->lives < 5 && $this->phrase->numberLost() >= 1 && $this->phrase->numberLost() <= 5) {
+			for ($win = 1; $win <= $this->lives; $win++) {
+				$this->hearts .= $winHearts;
+			}
+			for ($lose = 1; $lose <= $this->phrase->numberLost(); $lose++) {
+				$this->hearts .= $loseHearts;
+			}
 	}
+	
+	$this->hearts .= "</ol></div>";
+	return $this->hearts;
+}
 
-	// Verifies whether player has won the game 
-	public function checkForWin() {
-		$arrayIntersect = array_intersect($this->phrase->selected, $this->phrase->getLetterArray()); 
-		if (count($arrayIntersect) == count($this->phrase->getLetterArray())) {
-				return true;
-		} else {
-				return false;
+// Checks whether player lost the game
+public function checkForLose() {
+	if ($this->phrase->numberLost() == 5) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+// Redirects to lose page if player lost game or to win page if player won game
+public function gameOver() {
+	if ($this->checkForLose()) {
+			header('Location: game_over.php');
+	} elseif ($this->checkForWin()) {
+			header('Location: game_win.php');
+	} 
+}
+
+// Verifies whether player has won the game 
+public function checkForWin() {
+	$arrayIntersect = array_intersect($this->phrase->selected, $this->phrase->getLetterArray()); 
+	
+	if (count($arrayIntersect) == count($this->phrase->getLetterArray())) {
+		return true;
+	} else {
+			return false;
 		}
 	}
 }
