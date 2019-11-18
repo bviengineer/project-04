1b243266854585d9d6453212cfc6710a40fdcfe5
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-11-18 12:03:10 -0500

    Update game_over.php
    -Update/Remove comments
    -Fix indenation

--- game_over.php
+++ game_over.php
@@ -1,8 +1,7 @@
 <?php 
 	session_start();
-	// session_destro();
 
-  // Resets the game
+  // Reset game -> resseting session variables
 	if (isset($_POST['start'])) {
     unset($_SESSION['selected']);
     unset($_SESSION['phrase']);
@@ -20,14 +19,13 @@
 	</head>
 
 	<body>
-<!-- <h1 id="game-over-message">Congratulations on guessing: "The adventure begins"</h1> -->
-<div class="main-container">
-			<h2 class="header">Phrase Hunter</h2>
-<h1 id="game-over-message">The phrase was: <?php echo $_SESSION['phrase']; ?>. Better luck next time!
-  <form action="play.php" method="POST">
-    <input id="btn__reset" type="submit" name="start" value="Play again!" />
-  </form>
-  </div>
-</h1>
+    <div class="main-container">
+     <h2 class="header">Phrase Hunter</h2>
+      <h1 id="game-over-message">The phrase was: <?php echo $_SESSION['phrase']; ?>. Better luck next time!
+      <form action="play.php" method="POST">
+        <input id="btn__reset" type="submit" name="start" value="Play again!" />
+      </form>
+    </div>
+    </h1>
 </body>
 </html>
