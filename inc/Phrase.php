3f76152e89e971a0d1e3de633228a32208180996
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-11-18 11:59:37 -0500

    Update Phrase.php
    -Update/remove comments
    -Fix indentation

--- inc/Phrase.php
+++ inc/Phrase.php
@@ -1,8 +1,11 @@
 <?php
-// Phrase.php to create a Phrase class to handle the phrases
+/*================================================================
+	Phrase.php creates Phrase class and handles the phrases
+=================================================================*/
+
 class Phrase 
 {
-	private $currentPhrase; //"Australia is known as the land Down Under";
+	private $currentPhrase;
 	private $selected = [];
 	public $randomPhrase = [
 		"Boldness be my friend",
@@ -16,54 +19,62 @@ class Phrase
 		"The adventure begins", 
 	];
 
-	public function __construct($currentPhrase = null, $selected = array() ) {
-		if (!empty($currentPhrase)) {
-			$this->currentPhrase = $currentPhrase;
-		}
-		if (!empty($selected)) {
-			$this->selected = $selected;
-		} 
-		if(!isset($currentPhrase)) {
-			$getPhrase = rand(0, count($this->randomPhrase)-1);
-			$this->currentPhrase = $this->randomPhrase[$getPhrase];
+// Consturctor method 
+public function __construct($currentPhrase = null, $selected = array() ) {
+	if (!empty($currentPhrase)) {
+		$this->currentPhrase = $currentPhrase;
+	}
+	if (!empty($selected)) {
+		$this->selected = $selected;
+	} 
+	if(!isset($currentPhrase)) {
+		$getPhrase = rand(0, count($this->randomPhrase)-1);
+		$this->currentPhrase = $this->randomPhrase[$getPhrase];
+	}
+}
+
+// Add the phrase to be guessed to the page as blank squares
+public function addPhraseToDisplay() {
+	$phraseToGuess = str_split(strtolower($this->currentPhrase));
+	$phraseDisplay = "<ul>";
+	
+	foreach ($phraseToGuess as $thePhrase) {
+		if ($thePhrase == " ") {
+			$phraseDisplay .= "<li class='space'>" . $thePhrase . "</li>";
+		} elseif (!in_array($thePhrase, $this->selected)) {
+				$phraseDisplay .= "<li class='hide letter'>" . $thePhrase . "</li>";
+		} elseif (in_array($thePhrase, $this->selected)) {
+				$phraseDisplay .= "<li class='show letter'>" . $thePhrase . "</li>";
 		}
 	}
-	// Add the phrase to be guessed to the page as blank squares
-	public function addPhraseToDisplay() {
-		$phraseToGuess = str_split(strtolower($this->currentPhrase));
-		$phraseDisplay = "<ul>";
-			foreach ($phraseToGuess as $thePhrase) {
-				if ($thePhrase == " ") {
-						$phraseDisplay .= "<li class='space'>" . $thePhrase . "</li>";
-				} elseif (!in_array($thePhrase, $this->selected)) {
-						$phraseDisplay .= "<li class='hide letter'>" . $thePhrase . "</li>";
-				} elseif (in_array($thePhrase, $this->selected)) {
-						$phraseDisplay .= "<li class='show letter'>" . $thePhrase . "</li>";
-				}
-			}
 		$phraseDisplay .= "</ul>";
 		return $phraseDisplay;
-	}
-	// Check selected letter against the current phrase
-	public function checkLetter($letter) {
-		// $parsedPhrase = array_unique(str_split(strtolower(str_replace(" ", "", $this->currentPhrase))));
-		//$theLetter = $letter[count($letter)-1]; // gets most recent keypress 
-		return in_array($letter, $this->getLetterArray());
-	}
-	//Will return the array of letter seletions using the __get magic method 
-	public function __get($selected) {
-		return $this->selected;
-	}
-	public function getCurrentPhrase() {
-		return $this->currentPhrase;
-	}
-	public function getLetterArray() {
-		$uniqueLetters = array_unique(str_split(strtolower(str_replace(" ", "", $this->currentPhrase))));
-		return $uniqueLetters;
-	}
-	// Calculates the number of incorrcet letter guesses
-	public function numberLost() {
-		$wrongGussess = array_diff($this->selected, $this->getLetterArray());
+}
+
+// Checks and returns user selected letter against the letters in the current phrase
+public function checkLetter($letter) {
+	return in_array($letter, $this->getLetterArray());
+}
+
+// Returns array of letter seletions using the __get magic method 
+public function __get($selected) {
+	return $this->selected;
+}
+
+// Returns the current phrase
+public function getCurrentPhrase() {
+	return $this->currentPhrase;
+}
+
+// Returns lowercase unique letters in the current phrase 
+public function getLetterArray() {
+	$uniqueLetters = array_unique(str_split(strtolower(str_replace(" ", "", $this->currentPhrase))));
+	return $uniqueLetters;
+}
+
+// Calculates the number of incorrcet letter guesses in nubmer form 
+public function numberLost() {
+	$wrongGussess = array_diff($this->selected, $this->getLetterArray());
 		return count($wrongGussess);
 	}
 }
