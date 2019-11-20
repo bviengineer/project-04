64c2b0aad454b0bfd9d7c773643743e44f9cd9b3
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-11-20 13:34:40 -0500

    Update script.js
    Update comment

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ let keypress = "";
 // Event listener to listen for user keypress 
 document.addEventListener('keydown', logKey);
 
-/*=====================================================================================================
+/*============================================================================================================
   > Function is called when the user presses a key on the keyboad.
   > The keypress is then assigned to the keypress variable.
   > e is the event and key is an attribute on the e event that holds the letter value from the keyboard
@@ -17,8 +17,8 @@ document.addEventListener('keydown', logKey);
   > The keypress text is then converted to lowercase
   > Then, the value or text stored in keypress, is compared to the value on the button on the page to see if
     there is a match
-  > If there is a match, the click event gets called on the button on the page and PHP handles the rest.
- =======================================================================================================*/
+  > If there is a match, a click event is emulated on the button which triggers the PHP side of the application
+ ===============================================================================================================*/
 function logKey(e) {
   keypress = e.key;
   for (let i=0; i <= buttons.length - 1; i++) {
