a9ab785421234f6c42299f5bb7005917c7c16771
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-11-20 13:23:30 -0500

    Update script.js
    function to handle keypress

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 
+// Get all buttons form the play.php page
 let buttons = document.getElementsByClassName('key');
-
-// Keyboard keys
-const keyboard = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 
-                  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 
-                  'z', 'x', 'c', 'v', 'b', 'n', 'm'];
 
 // Variable to hold user keypress
 let keypress = "";
@@ -12,17 +8,14 @@ let keypress = "";
 // Event listener to listen for user keypress 
 document.addEventListener('keydown', logKey);
 
-// Assigns user keypress to keypress varaible
+// Is called when the user presses a key and assigns user keypress event denoted by the parameter e, to the o keypress varaible
 function logKey(e) {
   keypress = e.key;
-  //console.log(e);
-   if(keyboard.indexOf(keypress)) {
-			//console.log(keypress);
-     // console.log(buttons);
-    for (i=0; i <= buttons.length; i++) {
-        // if (keypress.toLowerCase() == buttons[i].innerText)
-             console.log(buttons[i].innerText);
+  for (let i=0; i <= buttons.length; i++) {
+    let theButton = buttons[i].innerText;
+    keypress = keypress.toLocaleLowerCase();
+      if (keypress.toLowerCase() === theButton) {
+          buttons[i].click();
+      }
     }
   }
-}
-
