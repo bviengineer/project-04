aad9e9bc5a363eb6df0a55fc20aa13bdad86a8e4
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-11-20 13:33:33 -0500

    Update script.js
    Add detailed comment to page.

--- js/script.js
+++ js/script.js
@@ -8,10 +8,20 @@ let keypress = "";
 // Event listener to listen for user keypress 
 document.addEventListener('keydown', logKey);
 
-// Is called when the user presses a key and assigns user keypress event denoted by the parameter e, to the o keypress varaible
+/*=====================================================================================================
+  > Function is called when the user presses a key on the keyboad.
+  > The keypress is then assigned to the keypress variable.
+  > e is the event and key is an attribute on the e event that holds the letter value from the keyboard
+  > The buttons, stored in buttons variable above, are then looped through and one at a time, EACH ONE 
+    is assinged to the local variable keypress.
+  > The keypress text is then converted to lowercase
+  > Then, the value or text stored in keypress, is compared to the value on the button on the page to see if
+    there is a match
+  > If there is a match, the click event gets called on the button on the page and PHP handles the rest.
+ =======================================================================================================*/
 function logKey(e) {
   keypress = e.key;
-  for (let i=0; i <= buttons.length; i++) {
+  for (let i=0; i <= buttons.length - 1; i++) {
     let theButton = buttons[i].innerText;
     keypress = keypress.toLocaleLowerCase();
       if (keypress.toLowerCase() === theButton) {
